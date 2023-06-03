b39da8e4a23cdb562c18b80db7f82d947fc534b8
Rijan-N <134152417+Rijan-N@users.noreply.github.com>
2023-06-03 11:18:53 +0545

    Updated update_weather.php

--- update_weather.php
+++ update_weather.php
@@ -1,10 +1,10 @@
 <?php
 
 // Create a connection to the MySQL database
-$servername = "sql209.epizy.com";
-$username = "epiz_34167501";
-$password = "vouzm8YwZWroW";
-$dbname = "epiz_34167501_weathers";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "weathers";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
